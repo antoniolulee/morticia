79fca23977dc7ff83f2f7fb905f1b0561159b8ea
Antonio Lu Lee <antoniolulee@MacBook-Air-de-Antonio.local>
2022-02-28 17:41:53 +0100

    install devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  
+  devise_for :users
   resources :operations
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
+  
   root "operations#index"
 
   get "/about", to:"pages#about"
