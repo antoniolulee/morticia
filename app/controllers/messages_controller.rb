65892f4a320ccab83326a3471def153c9ecaab48
Antonio Lu Lee <antoniolulee@MacBook-Air-de-Antonio.local>
2022-03-20 23:22:48 +0100

    finish messages feature

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,5 @@
 class MessagesController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_message, only: %i[ show edit update destroy ]
 
   # GET /messages or /messages.json
@@ -21,11 +22,15 @@ class MessagesController < ApplicationController
 
   # POST /messages or /messages.json
   def create
+    
     @message = Message.new(message_params)
+    
+    @message.user = current_user
+
 
     respond_to do |format|
       if @message.save
-        format.html { redirect_to message_url(@message), notice: "Message was successfully created." }
+        format.html { redirect_to operations_url, notice: "Message was successfully created." }
         format.json { render :show, status: :created, location: @message }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -65,6 +70,6 @@ class MessagesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def message_params
-      params.require(:message).permit(:content, :operation_id, :user_id)
+      params.require(:message).permit(:content, :operation_id)
     end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,9 @@
 class Message < ApplicationRecord
+  
+  validates :content, :operation_id, :user_id, presence: true 
+  validates :content, length: { minimum: 30, maximum: 300 }
+  
+  
   belongs_to :operation
   belongs_to :user
 end
