8df3efc7de825ad8d6a66ac6b4fa87777945cd28
Antonio Lu Lee <antoniolulee@MacBook-Air-de-Antonio.local>
2022-05-05 18:47:15 +0200

    change stripe credentials format

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,3 +1,7 @@
 # Add Stripe API Key
 
-Stripe.api_key = Rails.application.credentials.dig(Rails.env.to_sym, :stripe, :secret)
+#Stripe.api_key = Rails.application.credentials.dig(Rails.env.to_sym, :stripe, :secret)
+
+if Rails.application.credentials[:stripe].present? && Rails.application.credentials[:stripe][:secret].present?
+    Stripe.api_key = Rails.application.credentials[:stripe][:secret]
+  end
