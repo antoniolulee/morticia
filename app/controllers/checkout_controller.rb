f20c9b3b5fa281adf57787db2a32991cdcd1c345
Antonio Lu Lee <antoniolulee@MacBook-Air-de-Antonio.local>
2022-05-09 16:35:41 +0200

    add client reference id to track webhook

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -2,14 +2,18 @@ class CheckoutController < ApplicationController
 
 def create
 	
+	operation = Operation.find(params[:id])
 	session = Stripe::Checkout::Session.create({
 		payment_method_types: ['card'],
 		line_items: [{
-			name: 'Morticia',
+			description: operation.id,
+			name: "Morticia",
 			amount: 199,
 			currency: 'eur',
 			quantity: 1,
 		}],
+		
+		client_reference_id: operation.id,
 		mode: 'payment',
 		success_url: root_url,
 		cancel_url: your_operations_url,
--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -25,10 +25,8 @@ class WebhooksController < ApplicationController
       case event.type
       when 'checkout.session.completed'
         session = event.data.object
-        session_with_expand = Stripe::Checkout::Session.retrieve({ id: session.id, expand: ["line_items"]})
-        session_with_expand.line_items.data.each do |line_item|
-          @operation = Operation.find_by(stripe_product_id: line_item.price.product)
-          @operation.increment!(:sales_count)
+          @operation = Operation.find_by(operation_id: client_reference_id.to_i)
+          @operation.status = 1
         end
       end
   
